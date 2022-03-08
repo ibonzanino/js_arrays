a71eca058880a22337741b46e5b2f6562b731366
ibonzanino <bonzaninoigor@gmail.com>
2022-03-08 19:41:29 -0300

    7th challenge

---
+++ count_number_of_elements_in_javascript_array.js
@@ -0,0 +1,8 @@
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+function myFunction(a) { 
+
+  return a.length;
+  }
+
+console.log(myFunction([9,9,9]))
