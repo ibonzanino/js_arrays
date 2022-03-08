40c1fa3d4fa0023e81030af3b4a6b3d09bd3ecde
ibonzanino <bonzaninoigor@gmail.com>
2022-03-08 20:22:40 -0300

    10th challenge

---
+++ sort_an_array_of_strings_alphabetically.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array of strings as argument
+// Sort the array elements alphabetically
+// Return the result
+function myFunction(arr) { 
+
+  return arr.sort();
+//return arr.sort((a, b) => a.localeCompare(b));
+  }
+
+console.log(myFunction(['z', 'c', 'd', 'a', 'y', 'a', 'w']))
